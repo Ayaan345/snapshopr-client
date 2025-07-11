4a863c1bef7fbfe993b2cca55b193b021c6a177d
Ayaan345 <ayaangaming674@gmail.com>
2025-07-11 22:17:04 +0500

    Fix banner sizing: add width/height props to Next/Image

--- src/app/page.js
+++ src/app/page.js
@@ -37,7 +37,7 @@ export const revalidate = 60;
 export default async function Home() {
   return (
     <div className="min-h-screen w-full flex flex-col bg-gray-50 font-sans">
-      <main className="flex-1 max-w-full mx-auto px-4 mt-4"> {/* Changed max-w-7xl ml-8 to max-w-full mx-auto px-4 */}
+      <main className="flex-1 max-w-full  px-4 mt-4"> {/* Changed max-w-7xl ml-8 to max-w-full mx-auto px-4 */}
         {/* carousel*/}
         <BannerCarousel />
 
